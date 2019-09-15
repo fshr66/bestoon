4f5c5831b92a0af3ca54eecf7c0c36aa51431e67
fsh <farshid.sharifi@yahoo.com>
2019-09-15 09:44:09 +0430

    beautification

--- web/models.py
+++ web/models.py
@@ -7,4 +7,14 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length = 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
 
